a79ced245b93317be8d8cd7e2d4e1dfc4934ec14
Laura Train <laura.train@alumnos.uc3m.es>
2021-03-12 12:32:34 +0100

    pseudokalman added

---
+++ pseudo-Kalman/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/Users/Laura/STM32CubeIDE/workspace_1.4.0/first_try/Pseudo-Kalman/Core/Lib/libarm_cortexM4lf_math.a
+
+LIBS :=
+
